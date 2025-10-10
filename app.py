3bc3259adfb3b7134cd79b350aa6958e8890f3cb
dittostein <108161729+dittostein@users.noreply.github.com>
2025-10-09 19:15:07 -0700

    Set default port and disable debug

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def ping() -> object:
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get("PORT", "5001"))
+    app.run(debug=False, port=port)
